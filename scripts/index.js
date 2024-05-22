3aa9b4fa27a514f143d5580cdf0861e808f549cf
charlycharlie <charleytran101@gmail.com>
2024-05-21 18:31:39 -0700

    modal closes and opens properly

--- scripts/index.js
+++ scripts/index.js
@@ -26,4 +26,16 @@ const cardData = [
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     },
 ]; 
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector('#profile-edit-modal');
+const modalCloseButton = document.querySelector('#modal-close-button');
 
+profileEditButton.addEventListener('click',() => {
+    profileEditModal.classList.add('modal_opened');
+});
+
+modalCloseButton.addEventListener('click', () => {
+    profileEditModal.classList.remove('modal_opened')
+});
+ console.log(modalCloseButton);
+ console.log(profileEditModal);
